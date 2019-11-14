f426ec310a8b9b5dfbaf2b8fd3f7d9bc3f3e5167
zhongkai_mac <xuzhongkai_seu@icloud.com>
2019-11-14 13:34:24 +0800

    yaml

---
+++ shuidao_parse_yaml.py
@@ -0,0 +1,31 @@
+import yaml
+import os
+
+if __name__ == '__main__':
+    file_name = '水稻.txt'
+    path = os.path.join(os.getcwd(), file_name)
+
+    result = dict()
+    item = None
+    with open(path, 'r') as fr:
+        for line in fr.readlines():
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            words = line.split(' ', 1)
+            if len(words) == 1:
+                if item:
+                    name = item.pop('名称')
+                    result[name] = item
+                item = dict()
+                item['名称'] = words[0]
+            else:  # len(words) == 2:
+                prop = words[0]
+                value = words[1]
+                item[prop] = value.strip()
+
+        result[item['名称']] = item
+
+    w_path = os.path.join(os.getcwd(), '水稻.yml')
+    with open(w_path, 'w', encoding='utf-8') as fw:
+        yaml.dump(result, fw, allow_unicode=True)
